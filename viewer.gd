8e63c621938a527625ccabd808c56010931ad515
Tessimal <75651517+Z53-INCOMING@users.noreply.github.com>
2025-08-07 23:22:38 -0700

    camera settings

--- viewer.gd
+++ viewer.gd
@@ -4,6 +4,7 @@ var mouse_sensitivity := 0.004 * (2560.0 / float(ProjectSettings.get_setting("di
 
 @onready var camera = $Camera
 @onready var visual = $Visual
+@onready var ui = $UI
 
 var zoom := 1.0
 
@@ -13,7 +14,7 @@ var camera_fade_start := 3.5
 var camera_fade_end := 5.0
 var camera_w_fade_distance := 1.0
 var camera_w_fade_slope := 0.0
-var camera_w_fade_distance_focus := 1.0
+var camera_w_fade_distance_focus := 0.25
 var camera_w_fade_slope_focus := 0.0
 
 func _ready():
@@ -21,9 +22,18 @@ func _ready():
 	camera.projection_type = Camera4D.PROJECTION4D_PERSPECTIVE_4D
 
 func _process(delta):
-	camera.w_fade_distance = lerpf(camera.w_fade_distance, 0.5 if Input.is_action_pressed("focus") else 1.0, 1.0 - pow(2.0, -delta / 0.1))
+	camera.w_fade_distance = lerpf(camera.w_fade_distance, camera_w_fade_distance_focus if Input.is_action_pressed("focus") else camera_w_fade_distance, 1.0 - pow(2.0, -delta / 0.1))
+	camera.w_fade_slope = lerpf(camera.w_fade_slope, camera_w_fade_slope_focus if Input.is_action_pressed("focus") else camera_w_fade_slope, 1.0 - pow(2.0, -delta / 0.1))
 
 func _input(event):
+	if event is InputEventKey:
+		if event.keycode == KEY_ESCAPE and event.pressed:
+			ui.visible = !ui.visible
+			if ui.visible:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			else:
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			if Input.is_key_pressed(KEY_SHIFT):
@@ -37,7 +47,7 @@ func _input(event):
 				zoom *= 6.0 / 7.0
 		visual.material_override.line_thickness = line_thickness
 	
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and !ui.visible:
 		if Input.is_action_pressed("4d look"):
 			basis *= Basis4D.from_xw(event.relative.x * -mouse_sensitivity)
 			basis *= Basis4D.from_wy(event.relative.y * mouse_sensitivity)
@@ -49,3 +59,39 @@ func _input(event):
 			basis *= Basis4D.from_yz(event.relative.y * -mouse_sensitivity)
 	
 	visual.global_basis = Basis4D.from_scale(Vector4.ONE * zoom)
+
+func _camera_fade_start_changed(value):
+	camera_fade_start = value
+	update_camera()
+
+func _camera_fade_end_changed(value):
+	camera_fade_end = value
+	update_camera()
+
+func _camera_w_fade_distance_changed(value):
+	camera_w_fade_distance = value
+	update_camera()
+
+func _camera_w_fade_slope_changed(value):
+	camera_w_fade_slope = value
+	update_camera()
+
+func _camera_w_fade_distance_focus_changed(value):
+	camera_w_fade_slope_focus = value
+	update_camera()
+
+func _camera_w_fade_slope_focus_changed(value):
+	camera_w_fade_slope_focus = value
+	update_camera()
+
+func update_camera():
+	camera.depth_fade_start = camera_fade_start
+	if camera.depth_fade:
+		camera.clip_far = camera_fade_end
+	else:
+		camera.clip_far = 256.0
+
+
+func _depth_fade_toggled(toggled_on):
+	camera.depth_fade = toggled_on
+	update_camera()
