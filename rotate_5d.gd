008e3cb2d02b5173f2f573ec293f59c291599927
Tessimal <75651517+Z53-INCOMING@users.noreply.github.com>
2025-08-12 13:56:15 -0700

    finishing touches and v1.0.0

--- rotate_5d.gd
+++
@@ -1,8 +0,0 @@
-extends "res://mesh_instance_5d.gd"
-
-func _process(delta):
-	euler_5D.x += Input.get_axis("left", "right") * delta
-	euler_5D.y += Input.get_axis("down", "up") * delta
-	euler_5D.z += Input.get_axis("forward", "backward") * delta
-	euler_5D.w += Input.get_axis("kata", "ana") * delta
-	super(delta)
